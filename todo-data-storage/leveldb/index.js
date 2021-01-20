a524e351f9e17e8d82652bcd6ebf27f1344277b4
yamachan0625 <napoli.080625@gmail.com>
2021-01-21 01:01:01 +0900

    データストレージ levelDB

---
+++ todo-data-storage/leveldb/index.js
@@ -0,0 +1,67 @@
+'use strict';
+const level = require('level');
+const { join } = require('path');
+// 同じディレクトリ内のleveldbディレクトリにデータベースの状態を保存
+const db = level(join(__dirname, 'leveldb'));
+
+exports.fetchAll = async () => {
+  const result = [];
+  for await (const v of createValueStream({ gt: 'todo:', lt: 'todo;' })) {
+    result.push(JSON.parse(v));
+  }
+  return result;
+};
+
+exports.fetchByCompleted = async (completed) => {
+  const promises = [];
+  for await (const id of db.createValueStream({
+    gt: `todo-completed-${completed}:`,
+    lt: `todo-completed-${completed};`,
+  })) {
+    promises.push(db.get(`todo:${id}`).then(JSON.parse));
+  }
+  return Promise.all(promises);
+};
+
+exports.create = (todo) =>
+  db
+    .batch()
+    // ToDoの保存
+    .put(`todo:${todo.id}`, JSON.stringify(todo))
+    // セカンダリインデックスの保存
+    .put(`todo-completed-${todo.completed}:${todo.id}`, todo.id)
+    .write();
+
+exports.update = (id, update) =>
+  db.get(`todo:${id}`).then(
+    (content) => {
+      const oldTodo = JSON.parse(content);
+      const newTodo = {
+        ...oldTodo,
+        ...update,
+      };
+      let batch = db.batch().put(`todo:${id}`, JSON.stringify(newTodo));
+      if (oldTodo.completed !== newTodo.completed) {
+        batch = batch
+          .del(`todo-completed-${oldTodo.completed}:${id}`)
+          .put(`todo-completed-${newTodo.completed}:${id}`, id);
+      }
+      return batch.write();
+    },
+    // ToDoが存在しない場合はnullを返し、それ以外はそのままエラーにする
+    (err) => (err.notFount ? null : Promise.reject(err))
+  );
+
+exports.remove = (id) =>
+  db.get(`todo:${id}`).then(
+    (content) =>
+      db
+        .batch()
+        .del(`todo:${id}`)
+        .del(`todo-completed-true:${id}`)
+        .del(`todo-completed-false:${id}`)
+        .write()
+        .then(() => id),
+    // ToDoが存在しない場合はnullを返し、それ以外はそのままエラーにする
+    (err) => (err.cotFound ? null : Promise.reject(err))
+  );
